163a51ecef9fac690537d05266837975bfebd289
Urtzi_EU <urtzi.enriquez@gmail.com>
2021-05-19 13:27:09 +0200

    change results_output to 1 to run for the first time

--- run_Lacertas.m
+++ run_Lacertas.m
@@ -2,14 +2,13 @@ close all;
 global pets
 
 pets = {'Lacerta_schreiberi','Lacerta_agilis','Lacerta_strigata'};
-check_my_pet(pets);
 
 estim_options('default');
 estim_options('max_step_number', 1e3);
 estim_options('max_fun_evals',5e3);
 
 estim_options('pars_init_method', 2);
-estim_options('results_output', 4);
+estim_options('results_output', 1);
 estim_options('method', 'no');
 
 estim_pars;
