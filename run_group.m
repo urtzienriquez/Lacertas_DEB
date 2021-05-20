2c745b40d897da41a75f7f9c125e42eaac9c0c1d
Urtzi_EU <urtzi.enriquez@gmail.com>
2021-05-20 21:04:27 +0200

    exploring weights to z

--- run_group.m
+++ run_group.m
@@ -7,7 +7,7 @@ estim_options('default');
 estim_options('max_step_number', 1e3);
 estim_options('max_fun_evals',5e3);
 
-estim_options('pars_init_method', 2);
+estim_options('pars_init_method', 1);
 estim_options('results_output', 5);
 estim_options('method', 'nm');
 
